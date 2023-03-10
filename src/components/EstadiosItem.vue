26c13ff2a4dbc789258798b461da93d88f1fb463
Nicolas Batista Piñeyro <nicolas26386@gmail.com>
2023-03-10 14:08:34 -0300

    v1.38

--- src/components/EstadiosItem.vue
+++ src/components/EstadiosItem.vue
@@ -287,7 +287,7 @@ export default {
     .imagen-estadio-expandida
         height: 60vh
         margin-top: -35vh
-        padding: 1em 2em
+        padding: 1em 2em 1em 1.5em
         width: 95vw
 
     .estadios-grid
