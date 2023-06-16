5214066e080ae75c4b8ce3b5bfb71738b0892096
Nicolas Batista Piñeyro <nicolas26386@gmail.com>
2023-06-16 19:08:17 -0300

    v1.70

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+  build: {
+    target: 'esnext'
+  },
   plugins: [vue()],
   resolve: {
     alias: {
